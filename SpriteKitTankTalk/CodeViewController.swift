34edefab60ce686f120fe883c387f59604b0924a
Francesco Frison <ffrison@yammer-inc.com>
2015-09-04 14:17:32 +0100

    Add color to web view background as well as body ham programmatically

--- SpriteKitTankTalk/CodeViewController.swift
+++ SpriteKitTankTalk/CodeViewController.swift
@@ -26,19 +26,20 @@ class CodeViewController : UIViewController {
         }
     }
     
-    var webView =  WKWebView(frame: CGRectZero)
+    var webView =  WKWebView(frame: CGRectZero, configuration: CodeViewController.webViewConfiguration())
     var speakBoxXontroller : SpeakBoxController!
     
     var aboutToDismiss : (()->())?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.whiteColor()
+        self.view.backgroundColor = UIColor(red: (39 / 255), green: (40 / 255), blue: (34 / 255), alpha: 1.0)
         
         let(_, webViewRect) = self.view.bounds.divide(120, fromEdge: CGRectEdge.MaxYEdge)
         
         // Webview
         self.webView.frame = webViewRect
+        self.webView.backgroundColor = self.view.backgroundColor
         self.webView.autoresizingMask = [UIViewAutoresizing.FlexibleWidth, UIViewAutoresizing.FlexibleHeight]
         self.view.addSubview(self.webView)
     }
@@ -69,6 +70,26 @@ class CodeViewController : UIViewController {
         vc.navigationItem.rightBarButtonItem = closeBtn
         
         presenterViewController.presentViewController(navVC, animated: true, completion: nil)
+    }
+    
+    static func webViewConfiguration() -> WKWebViewConfiguration {
+        
+        let config = WKWebViewConfiguration()
+        
+        guard let path = NSBundle.mainBundle().pathForResource("InjectCSS", ofType: "js") else {
+            return config
+        }
+        
+        guard let scriptContent = try? String(contentsOfFile: path, encoding: NSUTF8StringEncoding) else {
+            return config
+        }
+        
+        
+        
+        let script = WKUserScript(source: scriptContent, injectionTime: .AtDocumentStart, forMainFrameOnly: true)
+        config.userContentController.addUserScript(script)
+
+        return config
     }
     
     func dismiss() {
---
+++ _etc/html/InjectCSS.js
@@ -0,0 +1,4 @@
+
+var styleTag = document.createElement("style");
+styleTag.textContent = 'body {background: #272822;}';
+document.documentElement.appendChild(styleTag);
