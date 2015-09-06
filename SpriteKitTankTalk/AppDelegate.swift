3498a68c35dce2ff62dbcdb9f098ec0a96f461a2
Francesco Frison <cescofry@gmail.com>
2015-09-06 20:42:50 +0100

    reinstablish correct mode

--- SpriteKitTankTalk/AppDelegate.swift
+++ SpriteKitTankTalk/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        self.window!.rootViewController = HtmlTestViewController.fromDummyVC()
+        //self.window!.rootViewController = HtmlTestViewController.fromDummyVC()
 
         
         return true
